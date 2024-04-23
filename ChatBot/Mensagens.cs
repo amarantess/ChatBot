e4502c288e653e78c0f3ea7ab5d1923f323b78c7
Gabriel Amarantes <gabrielamarantes13@gmail.com>
2024-04-23 11:23:34 -0300

    Classe SuporteTec e sua estrutura basica

--- ChatBot/Mensagens.cs
+++ ChatBot/Mensagens.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Deployment.Internal;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,29 +11,29 @@ namespace ChatBot
 {
     public class Mensagens
     {
+
         // Método para iniciar com as opções
         public void Iniciar()
         {
-            // Em breve adicionar mais opções
-            Inicio:
+
+        // Em breve adicionar mais opções
+        Inicio:
             Console.WriteLine("Olá. Você está no suporte online da Iluminado Fibra.");
-            Console.WriteLine("Para começarmos, escolha uma das opções abaixo: ");
+            Console.WriteLine("Escolha uma das opções abaixo:");
             Console.WriteLine("1: Preciso de suporte técnico");
             Console.WriteLine("2: Preciso de suporte financeiro");
             Console.WriteLine("3: Sair");
             int opcao = int.Parse(Console.ReadLine());
+            Console.Clear();
 
             switch (opcao)
             {
                 case 1:
-                    Console.WriteLine(); // Adicionar método Suporte Técnico
-                        break;
+                    new SuporteTec().ExecutarTec(); break;
                 case 2:
-                    Console.WriteLine(); // Adicionar método Suporte Financeiro
-                        break;
+                    Console.WriteLine(); break; // Adicionar método Suporte Financeiro
                 case 3:
-                    Console.WriteLine(); // Adicionar método Saida
-                        break;
+                    Console.WriteLine(); break; // Adicionar método Saida
                 default:
                     Console.WriteLine("Opção inválida. Tente novamente.");
                     goto Inicio;
